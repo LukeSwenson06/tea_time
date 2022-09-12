6d247acf0a3447c568a2cdb9ccb4f877025b565a
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-12 16:59:49 -0600

    Created database

---
+++ db/migrate/20220912225431_create_tea.rb
@@ -0,0 +1,10 @@
+class CreateTea < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+      t.string :brew_time
+      t.timestamps
+    end
+  end
+end
