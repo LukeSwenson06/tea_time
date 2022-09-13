038f62d241cefed5f1594c2c635997e5236d4891
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-13 11:17:37 -0600

    updated spelling errors

--- spec/requests/api/v1/customers/subscription_cancel_spec.rb
+++ spec/requests/api/v1/customers/subscription_cancel_spec.rb
@@ -1,10 +1,10 @@
-equire 'rails_helper'
+require 'rails_helper'
 
 describe 'subscription endpoint' do
     it 'subscribes a customer to a tea subscription' do    
         customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
         oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
-        subcription = Subscription.create(title: "monthly masterpiece collection", price: 127.0, frequency: :yearly, tea_id: oolong_tea.id, customer_id: customer.id)
+        subscription = Subscription.create(title: "monthly masterpiece collection", price: 127.0, frequency: :yearly, tea_id: oolong_tea.id, customer_id: customer.id)
     
         expect(subscription.status).to eq("active")
         
