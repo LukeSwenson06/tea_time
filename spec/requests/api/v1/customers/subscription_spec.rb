4c9863650e8e4be3a87319f4b20034b8a8990ff0
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-13 11:04:27 -0600

    added sad path testing for subscription

--- spec/requests/api/v1/customers/subscription_spec.rb
+++ spec/requests/api/v1/customers/subscription_spec.rb
@@ -5,26 +5,121 @@ describe 'subscription endpoint' do
         customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
         oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
 
-    params = {
-        tea_id: oolong_tea.id,
-        title: "monthly masterpiece collection",
-        price: "127",
-        frequency: :yearly
+        params = {
+            tea_id: oolong_tea.id,
+            title: "monthly masterpiece collection",
+            price: "127",
+            frequency: :yearly
+        }
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+        expect(response.status).to eq(201)
+
+        subscription = JSON.parse(response.body, symbolize_names: true)
+
+        expect(subscription[:data]).to have_key :id
+        attributes = subscription[:data][:attributes]
+        expect(attributes).to have_key :title
+        expect(attributes[:title]).to be_a String
+        expect(attributes).to have_key :price
+        expect(attributes[:price]).to be_a Float
+        expect(attributes).to have_key :frequency
+        expect(attributes[:frequency]).to be_a String
+        expect(attributes).to have_key :status
+        expect(attributes[:status]).to be_a String
+    end
+
+    it 'throws an error when request is missing a title' do
+        customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
+        oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
+        
+        params = {
+           
+                tea_id: oolong_tea.id,
+                price: "127",
+                frequency: :yearly
     }
-    post "/api/v1/customers/#{customer.id}/subscriptions", params: params
-    expect(response.status).to eq(201)
 
-    subscription = JSON.parse(response.body, symbolize_names: true)
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+    
+        expect(response.status).to eq(400)
+        
+        error = JSON.parse(response.body, symbolize_names: true)
+        expect(error[:error]).to eq("Title can't be blank")
+    end
+    
+    it 'throws an error when request is missing a price' do
+        customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
+        oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
+        
+        params = {
+           
+                tea_id: oolong_tea.id,
+                title: "monthly masterpiece collection", 
+                frequency: :yearly
+    }
 
-    expect(subscription[:data]).to have_key :id
-    attributes = subscription[:data][:attributes]
-    expect(attributes).to have_key :title
-    expect(attributes[:title]).to be_a String
-    expect(attributes).to have_key :price
-    expect(attributes[:price]).to be_a Float
-    expect(attributes).to have_key :frequency
-    expect(attributes[:frequency]).to be_a String
-    expect(attributes).to have_key :status
-    expect(attributes[:status]).to be_a String
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+    
+        expect(response.status).to eq(400)
+        
+        error = JSON.parse(response.body, symbolize_names: true)
+        expect(error[:error]).to eq("Price can't be blank")
+    end
+
+    it 'throws an error when request is missing a frequency' do
+        customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
+        oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
+        
+        params = {
+           
+                tea_id: oolong_tea.id,
+                title: "monthly masterpiece collection", 
+                price: "127"
+    }
+
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+    
+        expect(response.status).to eq(400)
+        
+        error = JSON.parse(response.body, symbolize_names: true)
+        expect(error[:error]).to eq("Frequency can't be blank")
+    end
+
+    it 'throws an error when request is missing a frequency' do
+        customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
+        oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
+        
+        params = {
+           
+                tea_id: oolong_tea.id,
+                title: "monthly masterpiece collection", 
+                price: "127"
+    }
+
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+    
+        expect(response.status).to eq(400)
+        
+        error = JSON.parse(response.body, symbolize_names: true)
+        expect(error[:error]).to eq("Frequency can't be blank")
+    end
+
+    it 'throws an error when request is missing a frequency' do
+        customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
+        oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
+        
+        params = {
+           
+                title: "monthly masterpiece collection", 
+                price: "127",
+                frequency: :yearly
+    }
+
+        post "/api/v1/customers/#{customer.id}/subscriptions", params: params
+    
+        expect(response.status).to eq(400)
+        
+        error = JSON.parse(response.body, symbolize_names: true)
+        expect(error[:error]).to eq("Tea must exist")
     end
 end
