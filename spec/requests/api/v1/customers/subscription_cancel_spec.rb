d833deaa76e971c3d31ad8c2db2e8683d4722edb
Luke Swenson <LukeSwenson95@outlook.com>
2022-09-13 12:41:07 -0600

    changed it block description

--- spec/requests/api/v1/customers/subscription_cancel_spec.rb
+++ spec/requests/api/v1/customers/subscription_cancel_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'subscription endpoint' do
-    it 'subscribes a customer to a tea subscription' do    
+    it 'cancels a customers tea subscription' do    
         customer = Customer.create(first_name: "Luke", last_name: "Swenson", email: "thebestemail@email.com", address: "nowhere road")
         oolong_tea = Tea.create(title: "Oolong Tea", description: "Partially oxidized tea between black and green tea", brew_time: "2 minutes")
         subscription = Subscription.create(title: "monthly masterpiece collection", price: 127.0, frequency: :yearly, tea_id: oolong_tea.id, customer_id: customer.id)
